6d7213a3b6e93d24332376eee8b78fd5b3a8f17b
cemilcengiz <cemilcengiz94@gmail.com>
2021-09-20 20:28:26 +0300

    Update caching the HF datasets fixtures

--- tests/scripts/cache_hf_dataset_fixtures.py
+++ tests/scripts/cache_hf_dataset_fixtures.py
@@ -1,8 +1,12 @@
+"""
+Caches the tests dataset to HuggingFace's `datasets` library's cache so that the
+interpreter can find it when we try to load it through the `datasets` library.
+"""
 from tests.scripts.utils import shell, validate_and_exit
 
 if __name__ == "__main__":
     validate_and_exit(
-        shell(
+        cache_test_fixtures=shell(
             "datasets-cli test test_data/hf_datasets/squad_qa_test_fixture --save_infos --all_configs"
         )
     )
