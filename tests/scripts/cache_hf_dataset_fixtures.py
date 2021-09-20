766e40e1e461148d93924dc8d5fe77e6a968fdfa
cemilcengiz <cemilcengiz94@gmail.com>
2021-09-20 14:53:21 +0300

    Update cache_hf_dataset_fixtures.py after test structure changes

--- tests/scripts/cache_hf_dataset_fixtures.py
+++ tests/scripts/cache_hf_dataset_fixtures.py
@@ -1,6 +1,8 @@
-from tests.scripts.utils import assert_shell
+from tests.scripts.utils import shell, validate_and_exit
 
 if __name__ == "__main__":
-    assert_shell(
-        "datasets-cli test test_data/hf_datasets/squad_qa_test_fixture --save_infos --all_configs"
+    validate_and_exit(
+        shell(
+            "datasets-cli test test_data/hf_datasets/squad_qa_test_fixture --save_infos --all_configs"
+        )
     )
