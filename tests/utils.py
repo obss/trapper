84c01475553354ca2154c7d57f3751543434c87c
Devrim <46989091+devrimcavusoglu@users.noreply.github.com>
2021-09-20 14:29:38 +0300

    Assertion on tests are changed to system exit. (#8)
    * Assertion on tests are changed to system exit.
    - Intentional failing test commit to check CI is OK with changes.
    
    * Assertion on tests are changed to system exit.
    - Intentional failing test commit to check CI is OK with changes.
    
    * Assertion on tests are changed to system exit.
    - Intentional failing test commit to check CI is OK with changes.
    
    * Tests and CI are OK.
    
    * Added summary message for failed components.

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,11 @@
 import os
+import shutil
+import sys
 
 
-def assert_shell(command, exit_status=0):
+def shell(command, exit_status=0):
     """
-    Run command through shell and return exit status if exit status of command run match with given exit status,
-    raise an error otherwise.
+    Run command through shell and return exit status if exit status of command run match with given exit status.
 
     Args:
         command: (str) Command string which runs through system shell.
@@ -14,7 +15,29 @@ def assert_shell(command, exit_status=0):
 
     """
     actual_exit_status = os.system(command)
-    assert (
-        exit_status == actual_exit_status
-    ), f"Unexpected exit code {str(actual_exit_status)}"
+    if actual_exit_status == exit_status:
+        return 0
     return actual_exit_status
+
+
+def validate_and_exit(expected_out_status=0, **kwargs):
+    if all([arg == expected_out_status for arg in kwargs.values()]):
+        # Expected status, OK
+        sys.exit(0)
+    else:
+        # Failure
+        print_console_centered("Summary Results")
+        fail_count = 0
+        for component, exit_status in kwargs.items():
+            if exit_status != expected_out_status:
+                print(f"{component} failed.")
+                fail_count += 1
+        print_console_centered(
+            f"{len(kwargs)-fail_count} success, {fail_count} failure"
+        )
+        sys.exit(1)
+
+
+def print_console_centered(text: str, fill_char="="):
+    w, _ = shutil.get_terminal_size((80, 20))
+    print(f" {text} ".center(w, fill_char))
