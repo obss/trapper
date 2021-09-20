3e1c37215b96c5772f7836249588f9478fd40452
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-09-20 14:22:17 +0300

    Added summary message for failed components.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,7 +1,7 @@
 from tests.utils import shell, validate_and_exit
 
 if __name__ == "__main__":
-    sts_test = shell(
+    sts_tests = shell(
         "pytest --cov trapper --cov-report term-missing --cov-report xml -vvv tests"
     )
-    validate_and_exit(sts_test)
+    validate_and_exit(tests=sts_tests)
--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 
 
@@ -19,10 +20,24 @@ def shell(command, exit_status=0):
     return actual_exit_status
 
 
-def validate_and_exit(*args, expected_out_status=0):
-    if all([arg == expected_out_status for arg in args]):
+def validate_and_exit(expected_out_status=0, **kwargs):
+    if all([arg == expected_out_status for arg in kwargs.values()]):
         # Expected status, OK
         sys.exit(0)
     else:
         # Failure
+        print_console_centered("Summary Results")
+        fail_count = 0
+        for component, exit_status in kwargs.items():
+            if exit_status != expected_out_status:
+                print(f"{component} failed.")
+                fail_count += 1
+        print_console_centered(
+            f"{len(kwargs)-fail_count} success, {fail_count} failure"
+        )
         sys.exit(1)
+
+
+def print_console_centered(text: str, fill_char="="):
+    w, _ = shutil.get_terminal_size((80, 20))
+    print(f" {text} ".center(w, fill_char))
