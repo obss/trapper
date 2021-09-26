478be9423cc55c2bd5d649a4af41245000ab3ee7
cemilcengiz <cemilcengiz94@gmail.com>
2021-09-26 16:57:59 +0300

    Expose data and model classes in their package's __init__ files

--- trapper/models/__init__.py
+++ trapper/models/__init__.py
@@ -1,6 +1,11 @@
 from trapper.models.auto_wrappers import (
     TransformerModelForCausalLM,
+    TransformerModelForMaskedLM,
+    TransformerModelForMultipleChoice,
+    TransformerModelForNextSentencePrediction,
     TransformerModelForQuestionAnswering,
+    TransformerModelForSeq2SeqLM,
+    TransformerModelForSequenceClassification,
     TransformerModelForTokenClassification,
 )
 from trapper.models.model import TransformerModel
