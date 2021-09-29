a0d122549ef149caf0a9afa1f286e3e085e63fb2
cemilcengiz <cemilcengiz94@gmail.com>
2021-09-29 14:43:09 +0300

    Rename test_data to test_fixtures

--- tests/scripts/cache_hf_dataset_fixtures.py
+++ tests/scripts/cache_hf_dataset_fixtures.py
@@ -7,6 +7,6 @@ from tests.scripts.utils import shell, validate_and_exit
 if __name__ == "__main__":
     validate_and_exit(
         cache_test_fixtures=shell(
-            "datasets-cli test test_data/hf_datasets/squad_qa_test_fixture --save_infos --all_configs"
+            "datasets-cli test test_fixtures/hf_datasets/squad_qa_test_fixture --save_infos --all_configs"
         )
     )
