c5ce1d960abb8edd3a3d9422be91e5317c90bc4d
cemilcengiz <cemilcengiz94@gmail.com>
2021-10-02 00:09:17 +0300

    Remove TrapperTestCase class since it is used by just a single class.

--- tests/common/test_plugins.py
+++ tests/common/test_plugins.py
@@ -1,34 +1,32 @@
+import pytest
 from allennlp.common.util import pushd
-from overrides import overrides
 
 from trapper.common.plugins import discover_plugins, import_plugins
-from trapper.common.testing.test_case import TrapperTestCase
 from trapper.data import DatasetLoader
 
 
-class TestPlugins(TrapperTestCase):
-    @overrides
-    def setup_method(self):
-        super().setup_method()
-        self.plugins_root = self.FIXTURES_ROOT / "plugins"
+@pytest.fixture(scope="module")
+def plugins_root(fixtures_root):
+    return fixtures_root / "plugins"
 
-    def test_no_plugins(self):
+
+def test_no_plugins(plugins_root):
+    available_plugins = set(discover_plugins())
+    assert available_plugins == set()
+
+
+def test_file_plugin(plugins_root):
+    test_no_plugins(plugins_root)
+
+    with pushd(plugins_root):
         available_plugins = set(discover_plugins())
-        assert available_plugins == set()
+        assert available_plugins == {"custom_dummy_package", "custom_dummy_module"}
 
-    def test_file_plugin(self):
-        self.test_no_plugins()
-
-        with pushd(self.plugins_root):
-            available_plugins = set(discover_plugins())
-            assert available_plugins == {"custom_dummy_package",
-                                         "custom_dummy_module"}
-
-            import_plugins()
-            dataset_loaders_available = DatasetLoader.list_available()
-            for name in (
-                    "dummy_dataset_loader_inside_package",
-                    "dummy_dataset_loader_inside_package2",
-                    "dummy_dataset_loader_inside_module",
-            ):
-                assert name in dataset_loaders_available
+        import_plugins()
+        dataset_loaders_available = DatasetLoader.list_available()
+        for name in (
+            "dummy_dataset_loader_inside_package",
+            "dummy_dataset_loader_inside_package2",
+            "dummy_dataset_loader_inside_module",
+        ):
+            assert name in dataset_loaders_available
