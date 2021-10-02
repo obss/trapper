fa9f7c346f5a1d5483eb711ab51195fb3968c65d
cemilcengiz <cemilcengiz94@gmail.com>
2021-10-02 11:03:04 +0300

    Remove tempdir fixture from trapper.tests since the tmpdir_factory from pytest is used instead of it.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,3 @@
-import shutil
-import tempfile
 from pathlib import Path
 
 import pytest
@@ -10,17 +8,10 @@ _FIXTURES_ROOT = _PROJECT_ROOT / "test_fixtures"
 
 
 @pytest.fixture(scope="package")
-def test_root():
+def tests_root():
     return _TESTS_ROOT
 
 
 @pytest.fixture(scope="package")
 def fixtures_root():
     return _FIXTURES_ROOT
-
-
-@pytest.fixture
-def tempdir():
-    dir_path = Path(tempfile.mkdtemp(prefix="test"))
-    yield dir_path
-    shutil.rmtree(dir_path)
