a13d3f8f4f4d129c31cab000afd364dda55101e3
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-11-03 15:05:58 +0300

    Experiment config fetch added.

--- trapper/common/notebook_utils/file_transfer.py
+++ trapper/common/notebook_utils/file_transfer.py
@@ -1,14 +1,26 @@
 import os
 import urllib.request
 from pathlib import Path
+from typing import Optional
 
 
-def download_from_url(from_url: str, to_path: str):
+def download_from_url(url: str, to_path: Optional[str] = None):
+    """
+
+    Args:
+        url:
+        to_path:
+
+    Returns:
+
+    """
+    if to_path is None:
+        to_path = os.path.basename(url)
 
     Path(to_path).parent.mkdir(parents=True, exist_ok=True)
 
     if not os.path.exists(to_path):
         urllib.request.urlretrieve(
-            from_url,
+            url,
             to_path,
         )
--- trapper/common/notebook_utils/prepare_data.py
+++ trapper/common/notebook_utils/prepare_data.py
@@ -8,9 +8,19 @@ SQUAD_QA_FIXTURES = {
     "train.json": "https://raw.githubusercontent.com/obss/trapper/main/test_fixtures/hf_datasets/squad_qa_test_fixture/train.json",
     "squad_qa_test_fixture.py": "https://raw.githubusercontent.com/obss/trapper/main/test_fixtures/hf_datasets/squad_qa_test_fixture/squad_qa_test_fixture.py",
 }
+EXPERIMENT_CONFIG = "https://raw.githubusercontent.com/obss/trapper/main/examples/question_answering/experiment.jsonnet"
 
 
 def download_fixture_data():
     for file, url in SQUAD_QA_FIXTURES.items():
         destination = os.path.join(FIXTURES_PATH, file)
         download_from_url(url, destination)
+
+
+def fetch_experiment_config():
+    download_from_url(EXPERIMENT_CONFIG)
+
+
+def prepare_data():
+    download_fixture_data()
+    fetch_experiment_config()
