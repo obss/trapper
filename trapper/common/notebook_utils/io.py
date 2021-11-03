84d9817bf6f5a26d8a257891b2b4aebbcbe8454c
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-11-03 14:42:47 +0300

    Code formatting.

--- trapper/common/notebook_utils/io.py
+++ trapper/common/notebook_utils/io.py
@@ -1,5 +1,5 @@
 import json
-from typing import List, Dict
+from typing import Dict, List
 
 
 def save_json(samples: List[Dict], path: str):
--- trapper/common/notebook_utils/prepare_data.py
+++ trapper/common/notebook_utils/prepare_data.py
@@ -4,13 +4,13 @@ from trapper.common.notebook_utils.file_transfer import download_from_url
 
 FIXTURES_PATH = "squad_qa_test_fixture"
 SQUAD_QA_FIXTURES = {
-	"dev.json": "https://raw.githubusercontent.com/obss/trapper/main/test_fixtures/hf_datasets/squad_qa_test_fixture/dev.json",
-	"train.json": "https://raw.githubusercontent.com/obss/trapper/main/test_fixtures/hf_datasets/squad_qa_test_fixture/train.json",
-	"squad_qa_test_fixture.py": "https://raw.githubusercontent.com/obss/trapper/main/test_fixtures/hf_datasets/squad_qa_test_fixture/squad_qa_test_fixture.py"
+    "dev.json": "https://raw.githubusercontent.com/obss/trapper/main/test_fixtures/hf_datasets/squad_qa_test_fixture/dev.json",
+    "train.json": "https://raw.githubusercontent.com/obss/trapper/main/test_fixtures/hf_datasets/squad_qa_test_fixture/train.json",
+    "squad_qa_test_fixture.py": "https://raw.githubusercontent.com/obss/trapper/main/test_fixtures/hf_datasets/squad_qa_test_fixture/squad_qa_test_fixture.py",
 }
 
 
 def download_fixture_data():
-	for file, url in SQUAD_QA_FIXTURES.items():
-		destination = os.path.join(FIXTURES_PATH, file)
-		download_from_url(url, destination)
+    for file, url in SQUAD_QA_FIXTURES.items():
+        destination = os.path.join(FIXTURES_PATH, file)
+        download_from_url(url, destination)
