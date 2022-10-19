56419e507dd653659bcb60ae319f6ad62beafa41
devrimcavusoglu <devrimcavusoglu@gmail.com>
2022-10-19 13:31:32 +0300

    create_pipeline_from_params() added to __init__ for pipeline component.

--- trapper/pipelines/__init__.py
+++ trapper/pipelines/__init__.py
@@ -1,4 +1,7 @@
-from trapper.pipelines.functional import create_pipeline_from_checkpoint
+from trapper.pipelines.functional import (
+    create_pipeline_from_checkpoint,
+    create_pipeline_from_params,
+)
 from trapper.pipelines.pipeline import ArgumentHandler, Pipeline
 from trapper.pipelines.question_answering_pipeline import (
     QuestionAnsweringArgumentHandler,
