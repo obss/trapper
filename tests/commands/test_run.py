a5fd58358968641d58ae82bcaeaeb0af30846a40
Ulaş "Sophylax" Sert <sophylax@sophylax.net>
2023-01-06 09:30:14 +0300

    Formatting

--- tests/commands/test_run.py
+++ tests/commands/test_run.py
@@ -1,27 +1,37 @@
-import pytest
 import sys
 from unittest.mock import patch
-from trapper.commands import main
+
+import pytest
+
 from trapper import FIXTURES_ROOT
+from trapper.commands import main
 
 COMMAND_FIXTURES = FIXTURES_ROOT / "commands"
 
+
 def test_run_command_missing(capsys):
-    with pytest.raises(SystemExit, match='2'):
+    with pytest.raises(SystemExit, match="2"):
         run_args = ["trapper", "run"]
-        with patch.object(sys, 'argv', run_args):
-            main('trapper')
+        with patch.object(sys, "argv", run_args):
+            main("trapper")
 
     captured = capsys.readouterr()
-    assert "trapper run: error: the following arguments are required" in captured.err
+    assert (
+        "trapper run: error: the following arguments are required" in captured.err
+    )
 
 
 def test_run_command_trivial(tmp_path):
-    run_args = ["trapper", "run", str(COMMAND_FIXTURES / "experiment_trivial.jsonnet"), "-o"]
+    run_args = [
+        "trapper",
+        "run",
+        str(COMMAND_FIXTURES / "experiment_trivial.jsonnet"),
+        "-o",
+    ]
     overrides = {
         "args.output_dir": str(tmp_path / "output"),
         "args.result_dir": str(tmp_path / "output"),
     }
     run_args.append(str(overrides))
-    with patch.object(sys, 'argv', run_args):
-        main('trapper')
+    with patch.object(sys, "argv", run_args):
+        main("trapper")
