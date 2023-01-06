248be7f074e39f346d57c2146cabee4d116f859b
Ulaş "Sophylax" Sert <sophylax@sophylax.net>
2023-01-06 04:20:36 +0300

    Add Simple Test for `run` Command

---
+++ tests/commands/test_run.py
@@ -0,0 +1,13 @@
+import pytest
+import sys
+from unittest.mock import patch
+from trapper.commands import main
+
+def test_run_command(capsys):
+    with pytest.raises(SystemExit, match='2'):
+        run_args = ["trapper", "run"]
+        with patch.object(sys, 'argv', run_args):
+            main('trapper')
+
+    captured = capsys.readouterr()
+    assert "trapper run: error: the following arguments are required" in captured.err
